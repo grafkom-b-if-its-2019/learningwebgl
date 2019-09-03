9ad33f89dd771a4ab47db31f003c1aee0eb42ea5
Hadziq 哈明飛 <hadziq@gmail.com>
2019-09-03 18:00:18 +0700

    Try to put glsl on files

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 (function() {
 
-  glUtils.SL.sourceFromHtml();
-  var VSHADER_SOURCE = glUtils.SL.Shaders.v1.vertex;
-  var FSHADER_SOURCE = glUtils.SL.Shaders.v1.fragment;
+  glUtils.SL.init({ callback: function() { main(); } });
 
-  var canvas = document.getElementById("glcanvas");
-  var gl = glUtils.checkWebGL(canvas);
-
-  // Inisialisasi shaders dan program
-  var vertexShader = glUtils.getShader(gl, gl.VERTEX_SHADER, VSHADER_SOURCE);
-  var fragmentShader = glUtils.getShader(gl, gl.FRAGMENT_SHADER, FSHADER_SOURCE);
-  var program = glUtils.createProgram(gl, vertexShader, fragmentShader);
-
-  gl.useProgram(program);
-
-  // Bersihkan layar jadi hitam
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
-  // Bersihkan buffernya canvas
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  gl.drawArrays(gl.POINTS, 0, 1);
+  function main() {
+    var canvas = document.getElementById("glcanvas");
+    var gl = glUtils.checkWebGL(canvas);
+  
+    // Inisialisasi shaders dan program
+    var vertexShader = glUtils.getShader(gl, gl.VERTEX_SHADER, glUtils.SL.Shaders.v1.vertex);
+    var fragmentShader = glUtils.getShader(gl, gl.FRAGMENT_SHADER, glUtils.SL.Shaders.v1.fragment);
+    var program = glUtils.createProgram(gl, vertexShader, fragmentShader);
+  
+    gl.useProgram(program);
+  
+    // Bersihkan layar jadi hitam
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  
+    // Bersihkan buffernya canvas
+    gl.clear(gl.COLOR_BUFFER_BIT);
+  
+    gl.drawArrays(gl.POINTS, 0, 1);
+  }
 
 })();
--- libs/glUtils.js
+++ libs/glUtils.js
@@ -1,7 +1,7 @@
 (function(global){
 
   var glUtils = {
-    VERSION: '0.0.2',
+    VERSION: '0.0.4',
     checkWebGL: function(canvas) {
       var contexts = ["webgl", "experimental-webgl", "webkit-3d", "moz-webgl"];
       var gl;
@@ -95,6 +95,80 @@
           }
           this.Shaders[version][shader.getAttribute(this.dataType)] = source;
         }
+      },
+      /*
+       * Ajax stuff
+       */
+      XMLHttpFactories: [
+        function () { return new XMLHttpRequest() },
+        function () { return new ActiveXObject("Msxml2.XMLHTTP") },
+        function () { return new ActiveXObject("Msxml3.XMLHTTP") },
+        function () { return new ActiveXObject("Microsoft.XMLHTTP") }
+      ],
+      createXMLHTTPObject: function() {
+        var xmlhttp = false;
+        for (var i = 0; i < this.XMLHttpFactories.length; i++) {
+          try {
+            xmlhttp = this.XMLHttpFactories[i]();
+          } catch (error) {
+            continue;
+          }
+          break;
+        }
+        return xmlhttp;
+      },
+      sendRequest: function(url, callback, element) {
+        var req = this.createXMLHTTPObject();
+        if (!req) return;
+        var method = "GET";
+        req.open(method, url, true);
+        req.onreadystatechange = function() {
+          if (req.readyState != 4) return;
+          if (req.status != 0 && req.status != 200 && req.status != 304) {
+            return;
+          }
+          callback(req, element);
+        }
+        if (req.readyState == 4) return;
+        req.send();
+      },
+      /*
+       * Signals
+       */
+      init: function(options) {
+        var options = options || {};
+        this.callback = options.callback || function() {};
+        this.elemName = options.elemName || "shader";
+        this.dataSrc = options.dataSrc || "data-src";
+        this.dataType = options.dataType || "data-type";
+        this.dataVersion = options.dataVersion || "data-version";
+        this.shaderElems = document.getElementsByName(this.elemName);
+        this.loadedSignal = new global.signals.Signal();
+        this.Shaders = this.Shaders || {};
+        this.loadedSignal.add(this.callback);
+        this.slShaderCount = this.shaderElems.length;
+        for(var i = 0; i < this.slShaderCount; i++) {
+          var shader = this.shaderElems[i];
+          this.sendRequest(shader.getAttribute(this.dataSrc), this.processShader, shader);
+        }
+        this.checkForComplete();
+      },
+      checkForComplete: function() {
+        if(!this.slShaderCount) {
+          this.loadedSignal.dispatch();
+        }
+      },
+      processShader: function(req,element) {
+        glUtils.SL.slShaderCount--;
+        var version = element.getAttribute(glUtils.SL.dataVersion);
+        if(!glUtils.SL.Shaders[version]) {
+          glUtils.SL.Shaders[version] = {
+            vertex: '',
+            fragment: ''
+          };
+        }
+        glUtils.SL.Shaders[version][element.getAttribute(glUtils.SL.dataType)] = req.responseText;
+        glUtils.SL.checkForComplete();
       }
     }
   };
