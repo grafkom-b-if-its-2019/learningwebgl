d2a8fd5bc25040cdce76343dc1d764a5ffce464a
Hadziq Fabroyir 哈明飛 <hadziq@gmail.com>
2019-11-19 08:12:32 +0700

    Pencahayaan dengan Fragment Shader

--- vertex.glsl
+++ vertex.glsl
@@ -5,6 +5,8 @@ attribute vec3 vColor;
 attribute vec3 vNormal;
 
 varying vec3 fColor;
+varying vec3 fPosition;
+varying vec3 fNormal;
 
 uniform mat4 projection;
 uniform mat4 view;
@@ -12,18 +14,11 @@ uniform mat4 model;
 
 uniform mat3 normalMatrix;
 
-uniform vec3 lightColor;
-uniform vec3 lightPosition;
-uniform vec3 ambientColor;
-
 void main() {
-  vec3 normal = normalize(normalMatrix * vNormal); // Supaya jadi unit vector
-  vec3 lightDirection = lightPosition - vec3(model * vec4(vPosition, 1.0));
-  float lightIntensity = max(dot(lightDirection, normal), 0.0);
-  vec3 diffuse = lightColor * vColor * lightIntensity;
-  vec3 ambient = ambientColor * vColor;
-  fColor = diffuse + ambient;
-
   gl_Position = projection * view * model * vec4(vPosition, 1.0);
   // urutan perkaliannya harus = projection x view x model (transformasi)
+
+  fColor = vColor;
+  fPosition = vec3(model * vec4(vPosition, 1.0));
+  fNormal = normalMatrix * vNormal;
 }
